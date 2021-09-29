34bbb49e6b269c13e2d4ad11b3ae6eed70cbe3b7
Manali <manalisdesai@gmail.com>
2021-09-29 07:38:40 +0530

    initial commit

---
+++ GithubCommitCommenAnalyzer/Controllers/GithubRepoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GithubCommitCommenAnalyzer.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GithubCommitCommenAnalyzer.Controllers
+{
+    public class GithubRepoController:Controller
+    {
+        private readonly ILogger<GithubRepoController> _logger;
+
+        public GithubRepoController(ILogger<GithubRepoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult FetchRepo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>FetchRepo(GithubRepo model)
+        {
+            if(ModelState.IsValid)
+            {
+
+
+
+
+
+            }
+            return View();
+        }
+
+        
+    }
+}
---
+++ GithubCommitCommenAnalyzer/Models/GithubRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GithubCommitCommenAnalyzer.Models
+{
+    public class GithubRepo
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string AccessToken { get; set; }
+
+        [Required]         
+        public string GithubURL { get; set; }
+
+    }
+}
